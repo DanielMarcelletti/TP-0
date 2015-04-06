b89a38f21c2df65bcb3976afd11a163108d886f6
Daniel Marcelletti <danimarche32@gmail.com>
2015-04-06 00:36:10 -0300

    Primera versión hello.c y salida.txt

---
+++ hello.c
@@ -0,0 +1,9 @@
+/* Escribe en pantalla la leyenda "Hello, World !" en pantalla
+* Daniel Marcelletti
+* 06/04/2015
+*/
+	#include <stdio.h>
+	int main(void) {
+	printf("Hello, World !\n");
+	return 0;
+}
